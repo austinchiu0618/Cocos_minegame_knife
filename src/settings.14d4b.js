8a95362db718b14cb57d00a47d10ad9134b9ff94
AustinChiu <78719537+austinchiu0618@users.noreply.github.com>
2021-08-18 03:21:51 +0800

    add files

---
+++ src/settings.14d4b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/sence/game.fire",orientation:"",jsList:[],bundleVers:{internal:"1b082",main:"13f43"}};
